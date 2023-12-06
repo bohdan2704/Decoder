f0d42bdbf2fcc2d35897ca1b8bb6013575511cd0
Bohdan <bohdan2704@gmail.com>
2023-12-06 18:56:00 +0200

    changing

--- src/main/java/org/example/Delete.java
+++ src/main/java/org/example/Delete.java
@@ -1,26 +1,8 @@
 package org.example;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileVisitOption;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.EnumSet;
 
 public class Delete {
-
-    public static void deleteFolder(String deleteFolder) {
-        try {
-            deleteFolderHelpful(Paths.get(deleteFolder));
-            System.out.println("Folder deleted successfully.");
-        } catch (IOException e) {
-            System.err.println("Error deleting folder: " + e.getMessage());
-        }
-    }
 
     public static void deleteFile(String folderPath) {
         try {
@@ -41,30 +23,6 @@ public class Delete {
             }
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
-        }
-    }
-
-    private static void deleteFolderHelpful(Path folderPath) throws IOException {
-        if (Files.exists(folderPath)) {
-            Files.walkFileTree(folderPath, EnumSet.noneOf(FileVisitOption.class), Integer.MAX_VALUE, new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    Files.delete(file);
-                    return FileVisitResult.CONTINUE;
-                }
-
-                @Override
-                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-                    // Handle the case where the visit of a file fails (optional)
-                    return FileVisitResult.CONTINUE;
-                }
-
-                @Override
-                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                    Files.delete(dir);
-                    return FileVisitResult.CONTINUE;
-                }
-            });
         }
     }
 }
